7e8825760f4e51021f15808a9f1fead9aecd3112
Sessa-Andrea <andrea.sessa@barsanti.edu.it>
2019-02-16 11:50:29 +0100

    caricamento2

--- verifica/verifica.ino
+++ verifica/verifica.ino
@@ -3,7 +3,8 @@ int ledwhite = 7;
 int ledyellow = 11;
 int ledred = 13;
 int tempo = 1000;
-
+int j = 0;
+int cicli;
 void setup() {
   // put your setup code here, to run once:
 pinMode(ledgreen,OUTPUT);
@@ -11,10 +12,14 @@ pinMode(ledwhite,OUTPUT);
 pinMode(ledyellow,OUTPUT);
 pinMode(ledred,OUTPUT);
 Serial.begin(9600);
-Serial.print("Inserisci durata dei led");
+Serial.println("Inserisci durata dei led");
 while(Serial.available() == 0){
 }
 tempo = Serial.readString().toInt();
+Serial.println("Inserisci quanti cicli deve fare");
+while(Serial.available() == 0){
+}
+cicli = Serial.readString().toInt() + 1;
 }
 void ledrossoacceso(){
   digitalWrite(ledred,HIGH);
@@ -47,9 +52,20 @@ void ledverdeacceso(){
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(j < cicli){
 ledrossoacceso();
 ledgialloacceso();
 ledbiancoacceso();
 ledverdeacceso();
+  j++;
+  }
+  if (j >= cicli - 1){
+  digitalWrite(ledred,LOW);
+  digitalWrite(ledyellow,LOW);
+  digitalWrite(ledwhite,LOW);
+  digitalWrite(ledgreen,LOW);
+ j = 0;
+  setup();//ricomincia
+    }
   
 }
